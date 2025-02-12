9d1f68b40332c71bde755886b502a0cde599c3c5
Rhayven Alano <rhayvenjonas.alano@lorma.edu>
2025-02-12 09:50:15 +0800

    Update Main.java

--- src/main/java/com/Rhayven/Main.java
+++ src/main/java/com/Rhayven/Main.java
@@ -10,21 +10,32 @@ public class Main {
 
         coffee1.displayInfo();
         System.out.println(coffee1.describe());
+        System.out.println();
 
         coffee1.discount(10);
         System.out.println("Price after discount: ₱" + coffee1.calculatePrice("Small"));
+        System.out.println();
 
         coffee1.addFlavor("Vanilla");
-        coffee1.updateStock(5);
+        coffee1.setDecaf(false);
+        coffee2.changeRoastLevel("Light");
+        coffee1.updateStock(-5);
         coffee1.displayInfo();
 
         System.out.println();
 
         coffee2.displayInfo();
         System.out.println(coffee2.describe());
+        System.out.println();
 
+        coffee2.discount(10);
+        System.out.println("Price after discount: ₱" + coffee2.calculatePrice("Medium"));
+        System.out.println();
+
+        coffee2.addFlavor("Cinnamon");
         coffee2.setDecaf(true);
-        coffee2.changeRoastLevel("Light");
+        coffee2.changeRoastLevel("Italian");
+        coffee2.updateStock(-2);
         coffee2.displayInfo();
     }
 }
