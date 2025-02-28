95f768f6daa091ee3488bbae4ffe6e408e70b171
Rhayven Pogi <rhayvenjonas.alano@lorma.edu>
2025-02-28 10:50:57 +0800

    Initial commit on branch springboot-list-view/delete

---
+++ src/main/java/com/rhayven/projectKapehan/ProjectKapehanApplication.java
@@ -0,0 +1,12 @@
+package com.rhayven.projectKapehan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKapehanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKapehanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rhayven/projectKapehan/ProjectKapehanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhayven.projectKapehan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKapehanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
