432db55e2065e7c3c769beea170f8767ec949d40
codeWmeku <mekudumaguin@gmail.com>
2025-03-12 09:50:45 +0800

    labwork

--- src/main/java/com/rhayven/projectKapehan/Coffee.java
+++ src/main/java/com/rhayven/projectKapehan/Coffee.java
@@ -3,6 +3,7 @@ package com.rhayven.projectKapehan;
 import java.util.List;
 
 public class Coffee {
+    //instance vars, non-static
     private int id;
     private String name;
     private String type;
@@ -15,6 +16,25 @@ public class Coffee {
     private List<String> flavorNotes;
     private String brewMethod;
 
+    //parameterless or default constructor
+    public Coffee(){
+
+    }
+
+    /**
+     * Constructor to accept property values
+     * @param id
+     * @param name
+     * @param type
+     * @param size
+     * @param price
+     * @param roastLevel
+     * @param origin
+     * @param isDecaf
+     * @param stock
+     * @param flavorNotes
+     * @param brewMethod
+     */
     public Coffee(int id, String name, String type, String size, double price, String roastLevel, String origin, boolean isDecaf, int stock, List<String> flavorNotes, String brewMethod) {
         this.id = id;
         this.name = name;
@@ -29,15 +49,59 @@ public class Coffee {
         this.brewMethod = brewMethod;
     }
 
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public String getType() { return type; }
-    public String getSize() { return size; }
-    public double getPrice() { return price; }
-    public String getRoastLevel() { return roastLevel; }
-    public String getOrigin() { return origin; }
-    public boolean isDecaf() { return isDecaf; }
-    public int getStock() { return stock; }
-    public List<String> getFlavorNotes() { return flavorNotes; }
-    public String getBrewMethod() { return brewMethod; }
+    public int getId() {
+        return id; }
+    public void setId(int id) {
+        this.id = id; }
+
+    public String getName() {
+        return name; }
+    public void setName(String name) {
+        this.name = name; }
+
+    public String getType() {
+        return type; }
+    public void setType(String type) {
+        this.type = type; }
+
+    public String getSize() {
+        return size; }
+    public void setSize(String size) {
+        this.size = size; }
+
+    public double getPrice() {
+        return price; }
+    public void setPrice(double price) {
+        this.price = price; }
+
+    public String getRoastLevel() {
+        return roastLevel; }
+    public void setRoastLevel(String roastLevel) {
+        this.roastLevel = roastLevel; }
+
+    public String getOrigin() {
+        return origin; }
+    public void setOrigin(String origin) {
+        this.origin = origin; }
+
+    public boolean isDecaf() {
+        return isDecaf; }
+    public void setDecaf(boolean isDecaf) {
+        this.isDecaf = isDecaf; }
+
+    public int getStock() {
+        return stock; }
+    public void setStock(int stock) {
+        this.stock = stock; }
+
+    public List<String> getFlavorNotes() {
+        return flavorNotes; }
+    public void setFlavorNotes(List<String> flavorNotes) {
+        this.flavorNotes = flavorNotes;
+    }
+
+    public String getBrewMethod() {
+        return brewMethod; }
+    public void setBrewMethod(String brewMethod) {
+        this.brewMethod = brewMethod; }
 }
