41f672f8174205f4df4eff392676db318c164661
Rhayven Pogi <rhayvenjonas.alano@lorma.edu>
2025-05-16 00:14:19 +0800

    login edit

--- src/main/java/com/rhayven/projectKapehan/controller/CoffeeController.java
+++ src/main/java/com/rhayven/projectKapehan/controller/CoffeeController.java
@@ -52,16 +52,16 @@ public class CoffeeController {
         return "index";
     }
 
-    @GetMapping("/menu")
-    public String menu(@RequestParam(defaultValue = "") String search,  HttpSession session, Model model) {
+    @GetMapping("/catalog")
+    public String catalog(@RequestParam(defaultValue = "") String search,  HttpSession session, Model model) {
         KapehanUser user= (KapehanUser) session.getAttribute("user");
         if(user == null) {
             return "redirect:/login";
         }
 
         model.addAttribute("coffeeList", coffeeService.searchCoffee(search));
-        model.addAttribute("activeMenu", "menu");
-        return "menu";
+        model.addAttribute("activeMenu", "catalog");
+        return "catalog";
     }
 
     /**
